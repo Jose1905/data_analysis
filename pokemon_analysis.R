d6c8643f1730162787b1701308bf54feead2b54b
David Vega <davarela231@gmail.com>
2025-03-20 22:52:16 -0600

    Project creation and general comments added

---
+++ pokemon_analysis.R
@@ -0,0 +1,17 @@
+#import required packages
+require(tidyverse)
+library(readr)
+
+#Store the dataset into a variable
+pokemonList <- read.csv("pokemon.csv")
+
+
+#Question #1: Who are the strongest non-legendary pokemon in terms of base stats? -----------------------------------
+
+
+
+#Question #2: What generation introduced the Pokemons with the highest average base stats? --------------------------
+
+
+
+#Draw a graph showing some of the results. --------------------------------------------------------------------------
